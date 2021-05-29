c3023d31a0e2a8d9238ab32408cbdbd6d2132e50
Daniel Steyn <http://Daniel.Steyn@stash.optimation.co.nz>
2021-05-29 15:55:13 +1000

    Smokeball Tests Solution.

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,10 +1,16 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
-};
-
+  // My Implementation
+  const results = urls.map(async url => {
+    const response = await httpGet(url)
+    const status = response.status
+    const message = JSON.parse(response.body).message
+    const result = status === 200 ? { "Arnie Quote": message } : { "FAILURE": message }
+    return result
+  })
+  return Promise.all(results)
+}
 module.exports = {
   getArnieQuotes,
 };
